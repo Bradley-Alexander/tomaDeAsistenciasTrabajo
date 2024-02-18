03f50b8544f451077c9b7e0382a4d2941e614c3e
Bradley Alexander Poma Vera <bradley.poma@unl.edu.ec>
2024-02-18 13:25:25 -0500

    Sistema Actualizado

--- APPasistencias/models.py
+++ APPasistencias/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100)
 
-# Create your models here.
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        abstract = True
+
+class Estudiante(Persona):
+    ciclo = models.PositiveIntegerField()
+    paralelo = models.CharField(max_length=10)
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=100)
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class Profesor(Persona):
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
+
+
+class Asistencia(models.Model):
+    fechaAsistencia = models.DateField()
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE, related_name="asistencia_list")
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
+    asistio = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.estudiante} - {self.fechaAsistencia}"
+    def registrarAsistencia(self, fecha, profesor, estudiante, materia, asistio):
+        self.fechaAsistencia = fecha
+        self.profesor = profesor
+        self.estudiante = estudiante
+        self.materia = materia
+        self.asistio = asistio
+        self.save()
+        return self
